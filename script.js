58e4c80af1bb4b934140c69666bf3be72a9ceb11
Andrew-Garcia <andrew.garcia.2015@gmail.com>
2019-07-13 22:52:52 -0500

    added button

--- script.js
+++ script.js
@@ -4,19 +4,16 @@ console.clear();
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new AudioContext();
 
-var oscillator = audioCtx.createOscillator();
-var gainNode = audioCtx.createGain();
-
-oscillator.type = 'square';
-oscillator.frequency.setValueAtTime(440, audioCtx.currentTime);
-
-oscillator.connect(gainNode).connect(audioCtx.destination);
-
-oscillator.start();
-gainNode.gain.value = 0;
-
+var AFreq = 440;
+var A = 9; // A's placement in the keys array
 var keys = ["C", "Db", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"];
 var majPent = [0, 2, 4, 7, 9];
+
+function getFrequencyOfNote(num)
+{
+	var halfStepDist = num - A;
+	return (AFreq * Math.pow(1.059463, halfStepDist)).toFixed(2);
+}
 
 function pickKey() {
 	return Math.floor(Math.random() * keys.length);
@@ -26,7 +23,7 @@ function getScaleNotes(scale, key) {
 	var scaleNotes = [];
 	for (i = 0; i < scale.length; i++)
 	{
-		scaleNotes.push(keys[(key + scale[i]) % keys.length]);
+		scaleNotes.push((key + scale[i]) % keys.length);
 	}
 	return scaleNotes;
 }
@@ -41,6 +38,25 @@ function randNoteSeq(scale, num) {
 }
 
 var scale = getScaleNotes(majPent, pickKey());
+var sequence = randNoteSeq(scale, 6);
 
 document.getElementById("p1").innerHTML = scale.toString();
-document.getElementById("p2").innerHTML = randNoteSeq(scale, 6).toString();
+document.getElementById("p2").innerHTML = sequence.toString();
+document.getElementById("p3").innerHTML = getFrequencyOfNote(sequence[0]);
+
+function playNoteSeq() {
+	var oscillator = audioCtx.createOscillator();
+	var gainNode = audioCtx.createGain();
+
+	var curTime = audioCtx.currentTime;
+
+	oscillator.type = 'square';
+	oscillator.frequency.setValueAtTime(440, curTime);
+	oscillator.frequency.setValueAtTime(311.13, curTime + 1);
+	oscillator.frequency.setValueAtTime(440, curTime + 2);
+
+	oscillator.connect(gainNode).connect(audioCtx.destination);
+
+	oscillator.start();
+	gainNode.gain.value = 0.01;
+}
